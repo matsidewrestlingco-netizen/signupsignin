f402ca9d5fc13c217d3b66909906f794e9387e98
matsidewrestlingco-netizen <matsidewrestlingco@gmail.com>
2025-12-09 09:43:28 -0500

    Update supabaseClient.js

--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+export const supabase = createClient(
+  "https://YOUR_PROJECT_ID.supabase.co",
+  "YOUR_PUBLIC_ANON_KEY"
+);
