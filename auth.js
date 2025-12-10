2e60768197a011e6d60044a9d6bb9247be6b153a
matsidewrestlingco-netizen <matsidewrestlingco@gmail.com>
2025-12-09 22:26:16 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,11 @@
+export function requireAdmin() {
+  const authed = localStorage.getItem("matside_admin");
+  if (authed !== "true") {
+    window.location.href = "admin-login.html";
+  }
+}
+
+export function logoutAdmin() {
+  localStorage.removeItem("matside_admin");
+  window.location.href = "admin-login.html";
+}
